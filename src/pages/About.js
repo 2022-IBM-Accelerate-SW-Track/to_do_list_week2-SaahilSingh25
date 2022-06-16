f7e7fb95144171ae9a271cef83cf2ea03322bd50
Saahil Singh <saahilsingh360@gmail.com>
2022-06-15 20:01:53 -0400

    Remove and date functions added

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,31 +1,26 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import "./About.css";
-import profile_pic from "../assets/profile_pic.jpg";
-
+import pfp from "../assets/pfp.jpeg";
+  
 export default class About extends Component {
   render() {
     return (
       <div>
-        {/* <p>Design your About me page </p> */}
         <div class="split left">
           <div className="centered">
-            <img
-              className="profile_image"
-              src={profile_pic}
-              alt="Profile Pic"
-            ></img>
+            <img className="profile_image" src={pfp} alt="Profile Pic"></img>
           </div>
         </div>
         <div className="split right">
           <div className="centered">
-            <div className="name_title">Your Name</div>
-            <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
+            <div className="name_title">Saahil Singh</div>
+            <div className="brief_description"> 
+              I am a sophomore studying Computer Science at the University of Maryland - College Park (GPA - 3.87). I am striving towards a career in software development. When working on projects, I can leverage my skills in Artificial Intelligence and front end development which I gained through my education and further developed through passion projects. Through these skills I hope to develop software that can improve the lives of people. Ideally in my career, I am looking to find the cross section between computer science and music. 
+              If you would like to contact me, I can be reached via email at: saahilsingh360@gmail.com
             </div>
           </div>
         </div>
       </div>
-    );
+    )
   }
 }
